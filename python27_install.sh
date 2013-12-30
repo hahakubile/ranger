cf2059e2f4eaa977d04f1bb3b76e2122930d7938
hahakubile <hahakubile@gmail.com>
2013-12-30 14:36:08 +0800

    Update python27_install.sh

--- python27_install.sh
+++ python27_install.sh
@@ -1,3 +1,4 @@
+# 1. install python
 wget http://www.python.org/ftp/python/2.7.5/Python-2.7.5.tgz -P /opt
 cd /opt
 tar xvf Python-2.7.5.tgz
@@ -9,5 +10,15 @@ make && make altinstall
 mv /usr/bin/python /usr/bin/python.bak
 ln -s /usr/local/bin/python2.7 /usr/bin/python
 
-# test
+# test install
 python
+
+# 2. install setuptools
+wget http://pypi.python.org/packages/source/s/setuptools/setuptools-0.6c11.tar.gz -P /opt/
+tar zxvf setuptools-0.6c11.tar.gz
+python setup.py install
+ 
+# 3. install pip
+easy_install pip
+
+# done
