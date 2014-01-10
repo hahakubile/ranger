fc6e0497c62e212b2aa534205ce3b12e6cca07c3
hahakubile <hahakubile@gmail.com>
2014-01-10 14:50:07 +0800

    Update python27_install.sh

--- python27_install.sh
+++ python27_install.sh
@@ -1,4 +1,6 @@
-# before you install python 2.7 in centos 5.*, first check https/zlib support
+# link: https://www.digitalocean.com/community/articles/how-to-set-up-python-2-7-6-and-3-3-3-on-centos-6-4
+
+# Before you install python 2.7 in centos 5.*, first check https/zlib/bzip2 support
 #
 # 1. Compression requires the (missing) zlib module
 # > yum -y install zlib zlib-devel
@@ -8,7 +10,11 @@
 # > yum -y install bzip2 bzip2-devel
 # 
 
-# 1. install python
+# When you are with CentOS 5.*, you will see that Python is at version 2.4.3, it is reserved for system's use.
+# Next we are going to install Python 2.7.5 without breaking the system's default.
+# It is rather important to not to get involved with that as critical system tools such as YUM depend on it. 
+
+# 1. install python 2.7
 wget http://www.python.org/ftp/python/2.7.5/Python-2.7.5.tgz -P /opt
 cd /opt
 tar xvf Python-2.7.5.tgz
@@ -17,24 +23,31 @@ cd Python-2.7.5
 # make altinstall is used to prevent replacing the default python binary file /usr/bin/python
 make && make altinstall
 
-# replace the default python
+# 2. Adding New Python Installation Location to $PATH
+export PATH="/usr/local/bin:$PATH"
+# or you can:
+# echo "export PATH=\"/usr/local/bin:\$PATH\"" >> /etc/profile
+# source /etc/profile
+
+# 3. Then check python command
+python2.7 -c "import sys; print sys.version"
+
+# optional, replace the default python, donot replace the default python unless you know what you are doing!
 mv /usr/bin/python /usr/bin/python.bak
 ln -s /usr/local/bin/python2.7 /usr/bin/python
-
-# test install
-python
-
-# restore yum
+# change yum to use python2.4
 vim /usr/bin/yum
 change #!/usr/bin/python to #!/usr/bin/python2.4
 
 # 2. install setuptools
-wget http://pypi.python.org/packages/source/s/setuptools/setuptools-0.6c11.tar.gz -P /opt/
-tar zxvf setuptools-0.6c11.tar.gz
-cd setuptools-0.6c11
-python setup.py install
+wget --no-check-certificate https://pypi.python.org/packages/source/s/setuptools/setuptools-1.4.2.tar.gz -P /opt
+tar xvf setuptools-1.4.2.tar.gz
+cd setuptools-1.4.2
+# Be careful, python2.7 here
+python2.7 setup.py install
  
 # 3. install pip
-easy_install pip
+curl https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python2.7 -
 
-# done
+# 4. install virtualenv
+pip install virtualenv
