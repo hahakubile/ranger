8a03829938155c2918ec44cebc9a4e3db912e94a
hahakubile <hahakubile@gmail.com>
2014-01-15 15:00:29 +0800

    Update scrapy_install.sh

--- scrapy_install.sh
+++ scrapy_install.sh
@@ -13,4 +13,7 @@ yum -y install libxml2 libxml2-devel libxslt libxslt-devel
 
 # 3. scrapy
 
+# @see http://stackoverflow.com/questions/7340784/easy-install-pyopenssl-error
+pip install pyOpenSSL==0.12
+
 pip install Scrapy==0.16.4
