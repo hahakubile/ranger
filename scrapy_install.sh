e3f6285e67840630d50410604d950900808b3cf2
hahakubile <hahakubile@gmail.com>
2014-01-15 14:46:09 +0800

    Add lxml & scrapy install

--- scrapy_install.sh
+++ scrapy_install.sh
@@ -1,7 +1,16 @@
-> Pre-requisites
-# 1. Python 2.7
-# 2. lxml 
-# 3. pip or easy_install
+# Pre-requisites
+# (1). Python 2.7
+# (2). lxml 
+# (3). pip or easy_install
 
+# 1. python & pip/easy_install
+# @see https://github.com/hahakubile/ranger/blob/master/python27_install.sh
 
-yum install libxml2 libxml2-devel libxslt libxslt-devel
+# 2. lxml
+# @see http://lxml.de/installation.html
+
+yum -y install libxml2 libxml2-devel libxslt libxslt-devel
+
+# 3. scrapy
+
+pip install scrapy
